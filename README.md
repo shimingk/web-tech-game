87374f9649cc978bdef0cb303ea5d0a5849e1caf
shiming <shmgkuang571@gmail.com>
2023-02-21 05:57:05 -0800

    game deployed

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # web-tech-game
-MP3
+MP3 - atari breakout
+
+<details><summary>write-up documentation</summary>
+
+1. learn and set up my web development toolchain
+2. Inspiration behind my game:
+    - Pong
+    - Peggle
+    - Atari Breakout
+3. Deciding on what external libraries to use for my game:
+    - Game engines:
+        - p5.play: ultmately p5.play was my choice due to the fact that it incorporates p5.js to render graphics
+        - pixi.js
+    - p5.js
+        - an obvious choice from prior experience (MP2)
+    - Three.js
+        - 3D graphics
+4. Implementing the Game
+    - Using p5.play library in my game
+        - using sprites for bricks: moving bricks adds a level of complexity to a typical pong game
+    - adjusting Window screen size for the game
+    - 
+5. Issue deep dive
+    - 
+6. Kudos to the following:
+    - This [Atari Breakout](https://workshops.hackclub.com/atari_breakout/) example game which I used as a scaffold
+    - This [Pong Game](https://editor.p5js.org/annawasson/sketches/BQFIoo6s2) p5.js sketch by [annawasson](https://editor.p5js.org/annawasson/sketches)
+    - Allison Parrish's p5.play [examples](https://creative-coding.decontextualize.com/making-games-with-p5-play/)
+    - p5.js [reference](https://p5js.org/reference/)
+    - p5.play [reference](https://p5play.org/learn/)
+</details>
---
+++ index.js
@@ -0,0 +1,200 @@
+let spr;
+
+// window.setup = () => {
+//   createCanvas(windowWidth, windowHeight);
+//   spr = createSprite(width / 2, height, 250, 50);
+//   spr.shapeColor = color(255);
+// };
+
+// window.draw = () => {
+//   background(50);
+//   spr.velocity.x = (mouseX - spr.position.x) * 0.2;
+//   spr.velocity.y = (mouseY - spr.position.y) * 0.2;
+//   drawSprites();
+// };
+
+// Window dimensions.
+const windowWidth = 1512;
+const windowHeight = 834;
+
+// Rows and columns.
+const rows = 1;
+const cols = 10 ;
+
+// Boolean for alive or not.
+let alive = true;
+
+// Dimensions for bricks
+const brickWidth =  Math.round(windowWidth / cols - 5);
+const brickHeight = Math.round((windowHeight * 1/10 ) / rows - 10);
+const innerWidth = 250;
+const innerHeight = 25;
+let innerLayer = [];
+let outerLayer = [];
+
+// Array to store brick and score
+let bricks = [];
+let score = 0;
+
+// PADDLE  
+let paddle = {
+  x: windowWidth / 2 - 25,
+  y: windowHeight - 25,
+  width: 250,
+  height: 25
+}
+
+// BALL
+let ball = {
+  x: paddle.x - 25,
+  y: paddle.y - 100,
+  speedX: 5,
+  speedY: 5,
+  diameter: 25,
+}
+
+// Set up the canvas.
+window.setup = () => {
+  createCanvas(windowWidth, windowHeight);
+  spr = createSprite(width / 2, height, 250, 50);
+  spr.shapeColor = color(255);
+  generateBricks();  
+}
+
+// Generate bricks.
+window.generateBricks = () => {
+  for(let i = 0; i < rows; i++) {
+    for(let j = 0; j < cols; j++) {
+      let brickData = {
+        x: j * (brickWidth + 2) + 10,
+        y: i * (brickHeight + 2) + 25,
+        width: brickWidth,
+        height: brickHeight
+      }
+      bricks.push(brickData);
+    }
+  }
+}
+
+// Draw bricks.
+window.drawBricks = () => {
+  bricks.forEach(brick => {
+    fill('springgreen');
+    rect(brick.x, brick.y + 50, brick.width, brick.height);
+    noStroke();
+  });
+}
+
+// Keyboard input.
+window.keyPressed = () => {
+
+  // restart game 
+  if(keyCode === 32 && !alive) {
+    alive = true;
+    paddle.x = windowWidth / 2 - 50,
+    ball.x = paddle.x - 25,
+    ball.y = paddle.y - 50,
+    ball.speedX = 6;
+    ball.speedY = 6;
+    bricks.splice(0, bricks.length); // clean array of bricks    
+    score = 0;
+    generateBricks();
+  }
+}
+
+// Draw the paddle.
+window.drawPaddle = () => {
+  fill('teal');
+  rect(mouseX - 125, paddle.y, paddle.width, paddle.height);
+}
+
+// Draw the ball.
+window.drawBall = () => {
+  fill('white');
+  circle(ball.x, ball.y, ball.diameter); 
+  // Collision on top of the screen
+  if(ball.y - ball.diameter / 2 <= 0) {
+    ball.speedY = -ball.speedY;
+  }
+  // Collision on the bottom of the screen.
+  if(ball.y + ball.diameter / 2 >= windowHeight) {
+    alive = false;    
+  }
+  // Collision on the left and right of the screen.
+  if(ball.x - ball.diameter / 2 <= 0  || ball.x + ball.diameter / 2 >= windowWidth) {
+    ball.speedX = -ball.speedX;
+  }
+  // Paddle collision for first half.
+  if(ball.y + ball.diameter / 2 >= paddle.y && ball.x >= mouseX - 125 && ball.x < mouseX - 125 + paddle.width / 2) {
+    ball.speedY = -ball.speedY;
+    if(ball.speedX > 0) {
+      ball.speedX = -ball.speedX;
+    }    
+  }
+  // Paddle collision for second half.
+  if(ball.y + ball.diameter / 2 >= paddle.y && ball.x >= mouseX - 125 + paddle.width / 2 && ball.x < mouseX - 125  + paddle.width) {
+    ball.speedY = -ball.speedY;
+    if(ball.speedX < 0) {
+      ball.speedX = -ball.speedX;
+    }    
+  }
+
+  // Brick collision.
+  bricks.forEach((brick, index) => {
+    if(ball.y - ball.diameter / 2 <= brick.y + 50 + brick.height && ball.x > brick.x && ball.x <= brick.x + brick.width) {
+      ball.speedY = -ball.speedY;
+      bricks.splice(index, 1);
+      score++;
+      if(bricks.length === 0) alive = false;
+    }
+  }); 
+
+  // Move the ball on the screen.
+  ball.x += ball.speedX;
+  ball.y += ball.speedY;
+}
+
+// Display score at the top of the screen.
+window.displayScore = () => {
+  fill("beige");
+  textAlign(CENTER);
+  textSize(20)
+  text(`Score: ${score}`, windowWidth / 2, 22);
+}
+
+// Display message (either "GAME OVER" or "You Win!")
+window.endScreen = (message) => {
+  if (message === "You Win!") {
+    fill('springgreen');
+    message += "  🤩";
+  } else {
+    fill('magenta');
+  }
+  textAlign(CENTER);
+  textSize(35);
+  text(message, windowWidth / 2, windowHeight / 2); // 300, 170
+  text('Play again: [Space]', windowWidth / 2, windowHeight / 2 + 55); // 300, 225
+  text(`Score: ${score}`, windowWidth / 2, windowHeight / 2 - 55); // 300, 280
+}
+
+// Animate and draw everything to the screen.
+window.draw = () => {
+  background("black");
+  spr.velocity.x = (mouseX - spr.position.x) * 0.2;
+  // If the player broke all the bricks, they win.
+  if(bricks.length === 0) {
+    endScreen("You Win!");
+  }
+  
+  // If the player died and there are still bricks to break, they lost.
+  if(!alive && bricks.length != 0) endScreen("GAME OVER 😜");
+  
+  // If the player is still alive, draw everything to the screen.
+  if(alive) {
+    drawBricks();
+    drawPaddle();
+    drawBall();
+    displayScore();
+    drawSprites();
+  }
+}
